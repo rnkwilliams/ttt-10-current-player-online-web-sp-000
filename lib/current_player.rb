eb3f8da44a09c0f8e7db7c10907e578187691d9c
Ranika Williams <rnkprice07@gmail.com>
2019-04-30 17:30:59 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if (position == "X")
+      counter += 1
+    end
+  end
+
+
+def current_player(board)
+  if (turn_count(board) % 2 == 0)
+    return "X"
+  else
+    return "O"
+  end
+end
